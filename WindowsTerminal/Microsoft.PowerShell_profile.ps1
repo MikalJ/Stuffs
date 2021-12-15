6d46f6b69bb80ae2dfe6d5c757556c323f6c0ae0
Mikal Schacht Suffolk Jensen <msj@remoni.dk>
2021-12-15 12:58:45 +0100

    Added zlocation

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 oh-my-posh --init --shell pwsh --config "C:\src\Stuffs\WindowsTerminal\Mikal.omp.json" | Invoke-Expression
-if ($host.Name -eq 'ConsoleHost')
-{
-    Import-Module PSReadLine
-}
 Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Import-Module ZLocation
+#Add-Content -Value "`r`n`r`nImport-Module ZLocation`r`n" -Encoding utf8 -Path $PROFILE.CurrentUserAllHosts
+Write-Host -Foreground Green "[ZLocation] knows about $((Get-ZLocation).Keys.Count) locations."
--- WindowsTerminal/Setup.ps1
+++ WindowsTerminal/Setup.ps1
@@ -1,3 +1,4 @@
 winget install JanDeDobbeleer.OhMyPosh --force
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Install-Module PSReadLine -AllowPrerelease -Force
+install-module ZLocation -force
