94ec4400276223d03e420980dffd3f4c2054a46e
Mikal <msj@remoni.dk>
2022-09-27 07:24:12 +0200

    Added some kubectl aliases to PS profile

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -7,3 +7,10 @@ Set-PSReadLineOption -EditMode Windows
 Import-Module ZLocation
 #Add-Content -Value "`r`n`r`nImport-Module ZLocation`r`n" -Encoding utf8 -Path $PROFILE.CurrentUserAllHosts
 Write-Host -Foreground Green "[ZLocation] knows about $((Get-ZLocation).Keys.Count) locations."
+
+# Kubectl aliases
+Function KubectlGetPods { kubectl get pods }
+Function KubectlClusterInfo { kubectl cluster-info }
+New-Alias -Force -Name k -Value kubectl
+New-Alias -Force -Name pods -Value KubectlGetPods
+New-Alias -Force -Name ci -Value KubectlClusterInfo
