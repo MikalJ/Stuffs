0e47e632644ef28fa7c72dec1965462ac0cdaf38
Mikal Schacht Suffolk Jensen <msj@remoni.dk>
2025-02-26 15:01:43 +0100

    Added b64 and split-string to profile

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -28,3 +28,55 @@ Function KubectlClusterInfo { kubectl cluster-info $args }
 New-Alias -Force -Name k -Value kubectl
 New-Alias -Force -Name pods -Value KubectlGetPods
 New-Alias -Force -Name ci -Value KubectlClusterInfo
+
+# Base 64
+function FromB64 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Base64String
+    )
+    process {
+        $mod = $Base64String.Length % 4
+        if ($mod -eq 2) {
+            $Base64String += '=='
+        } elseif ($mod -eq 3) {
+            $Base64String += '='
+        }
+        $decodedString = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Base64String))
+        Write-Output $decodedString
+    }
+}
+
+
+function ToB64 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$String
+    )
+    process {
+        $encodedString = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($String))
+        Write-Output $encodedString
+    }
+}
+
+function Split-String {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Input,
+        
+        [Parameter(Mandatory = $true, ValueFromPipeline = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Separator
+    )
+    process {
+        Foreach($item in $Input.Split($Separator, [System.StringSplitOptions]::RemoveEmptyEntries)) {
+            Write-Output $item
+        }
+    }
+}
