80550584e8891b825f92eeec3f2525e24ef9ab97
Mikal Schacht Suffolk Jensen <msj@remoni.dk>
2025-08-28 14:37:53 +0200

    Updated powershell profile

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -29,7 +29,6 @@ New-Alias -Force -Name k -Value kubectl
 New-Alias -Force -Name pods -Value KubectlGetPods
 New-Alias -Force -Name ci -Value KubectlClusterInfo
 
-
 function Sha256 {
     [CmdletBinding()]
     param (
@@ -41,17 +40,63 @@ function Sha256 {
         $byteArray = [System.Text.Encoding]::UTF8.GetBytes($String)
         $sha256 = [System.Security.Cryptography.SHA256]::Create()
         $hashBytes = $sha256.ComputeHash($byteArray)
-        $hashString = [BitConverter]::ToString($hashBytes) -replace '-'
-        Write-Output $hashString
+        Write-Output $hashBytes -NoEnumerate
     }
 }
+function Sha256B64 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$String
+    )
+    process {
+        $byteArray = [System.Text.Encoding]::UTF8.GetBytes($String)
+        $sha256 = [System.Security.Cryptography.SHA256]::Create()
+        $hashBytes = $sha256.ComputeHash($byteArray)
+        $base64String = [Convert]::ToBase64String($hashBytes)
+        Write-Output $base64String
+    }
+}
+
+function ToHex {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false, Position = 1, ValueFromPipeline = $true, HelpMessage = "Input as a byte array to encode.")]
+        [ValidateNotNullOrEmpty()]
+        [byte[]]$ByteArrayInput
+    )
+    process {
+        $hex = [System.Convert]::ToHexString(@($ByteArrayInput;))
+        Write-Output $hex
+    }
+}
+
+function FromHex {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 1, ValueFromPipeline = $true, HelpMessage = "Input as a string to decode.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Input
+    )
+    process {
+        $bytes = [System.Convert]::FromHexString($Input)
+        Write-Output $bytes -NoEnumerate
+    }
+}
+
+
+
 # Base 64
 function FromB64 {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [ValidateNotNullOrEmpty()]
-        [string]$Base64String
+        [string]$Base64String,
+        
+        [Parameter(Mandatory = $false)]
+        [switch]$AsByteArray
     )
     process {
         $mod = $Base64String.Length % 4
@@ -60,21 +105,41 @@ function FromB64 {
         } elseif ($mod -eq 3) {
             $Base64String += '='
         }
-        $decodedString = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Base64String))
-        Write-Output $decodedString
+        
+        $decodedBytes = [System.Convert]::FromBase64String($Base64String)
+        
+        if ($AsByteArray) {
+            Write-Output $decodedBytes -NoEnumerate
+        } else {
+            $decodedString = [System.Text.Encoding]::UTF8.GetString($decodedBytes)
+            Write-Output $decodedString
+        }
     }
 }
-
 
 function ToB64 {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$String
+        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true, HelpMessage = "Input as a string to encode.")]
+        [string]$StringInput,
+
+        [Parameter(Mandatory = $false, Position = 1, ValueFromPipeline = $true, HelpMessage = "Input as a byte array to encode.")]
+        [byte[]]$ByteArrayInput
     )
+
     process {
-        $encodedString = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($String))
+        # Handle if the input is a string
+        if ($PSCmdlet.MyInvocation.BoundParameters['StringInput']) {
+            $encodedString = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($StringInput))
+        }
+        # Handle if the input is a byte array
+        elseif ($PSCmdlet.MyInvocation.BoundParameters['ByteArrayInput']) {
+            $encodedString = [System.Convert]::ToBase64String($ByteArrayInput)
+        }
+        else {
+            throw "You must specify either a string or a byte array."
+        }
+
         Write-Output $encodedString
     }
 }
@@ -96,3 +161,8 @@ function Split-String {
         }
     }
 }
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
